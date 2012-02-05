49f16027c405b10643edd78c06f9303b9f95bfc5
Steven McGrath <steve@chigeek.com>
2012-02-05 17:17:03 -0600

    Initial config module rev

---
+++ baskit/config.py
@@ -0,0 +1,40 @@
+import os
+
+def get_config_file():
+    config_file = 'baskit.conf'
+    if not os.path.exists(config_file):
+        config_file = '%s/.baskit.conf' % os.environ['HOME']
+    if not os.path.exists(config_file):
+        config_file = '/etc/baskit.conf'
+    if not os.path.exists(config_file):
+        conf_file = open('baskit.conf', 'w')
+        conf_file.write(sample_config)
+        conf_file.close()
+        config_file = 'baskit.conf'
+    return config_file
+
+cample_config = '''
+[Server: default]
+java_args       = 
+environment     = /opt/minecraft
+binary          = server.jar
+server_type     = vanilla
+server_branch   = stable
+server_build    = 1.0
+worlds          = world, world_nether, world_the_end
+
+[World: world]
+environment     = /opt/minecraft
+ramdisk         = no
+automount       = no
+
+[World: world_nether]
+environment     = /opt/minecraft
+ramdisk         = no
+automount       = no
+
+[World: world_the_end]
+environment     = /opt/minecraft
+ramdisk         = no
+automount       = no
+'''
