a7fa058f3f010f2b73803f86bd9db548fd98b921
Steven McGrath <steve@chigeek.com>
2012-02-05 15:15:46 -0600

    Uncommented Spout downloader

--- baskit/mc/__init__.py
+++ baskit/mc/__init__.py
@@ -19,8 +19,8 @@ def download(server_type='vanilla', build_type='stable'):
         return vanilla.download(build_type)
     if server_type == 'bukkit':
         return bukkit.download(build_type)
-    #if server_type == 'spout':
-    #    return spout.download(build_type)
+    if server_type == 'spout':
+        return spout.download(build_type)
     #if server_type == 'canary':
     #    return canary.download(build_type)
     else:
