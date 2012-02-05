97cef6dee9d221a3377205550b986b164b1c57f9
Steven McGrath <steve@chigeek.com>
2012-02-05 14:43:26 -0600

    Added backup/recovery and snapshot/recovery

--- baskit/server.py
+++ baskit/server.py
@@ -1,11 +1,25 @@
 import os
 import sys
+import re
+from zipfile import ZipFile
 from commands import getoutput as run
 from ConfigParser import ConfigParser
 
 from world import World
 import mc
 import screen
+
+def compress_folder(zip_filename, location, from_loc=None, excludes=[]):
+    zfile = ZipFile(zip_filename, 'a')
+    for dirpath, dirs, files in os.walk(location):
+        for f in files:
+            excluded = False
+            fn = os.path.join(dirpath, f)
+            arc_name = fn.replace(from_loc, '')
+            if fn in excludes:
+                continue
+            zfile.write(fn, arc_name)
+    zfile.close()
 
 class Server(object):
     name = 'default'
@@ -37,7 +51,7 @@ class Server(object):
         '''
         return screen.exists('mc_%s' % self.name):
     
-    def get_config(self):
+    def get_config(self, config_file=None):
         '''get_config
         Fetches the configuration for the server and adjusts the variables
         within the server object to match the configuration.
@@ -45,7 +59,9 @@ class Server(object):
         
         # Initializing the configration parser and reading the config file.
         config = ConfigParser()
-        config.read(self._config_file)
+        if config_file == None:
+            config_file = self._config_file
+        config.read(config_file)
         
         # Setting the section name and fetching the information from the
         # configuration file and applying those values to the appropriate
@@ -228,4 +244,146 @@ class Server(object):
         
         return True # We have to return something so that we know it worked ;)
         
+    def world_backup(self, world_name, backup_name=None):
+        '''backup [world name], [backup_name]
+        Runs the backup procedure for the specified world.
+        '''
+        # If there is no name specified, then go ahead and create one using
+        # the current date & time.
+        if backup_name == None:
+            backup_name = '%s_%s' % (world_name, 
+                            datetime.datetime.now().strftime('%Y-%m-%d_%H%M'))
         
+        # Perform a save-all & save-off before we run the backup to write all
+        # changes to disk.
+        self.command('save-all')
+        self.command('save-off')
+        
+        # Now walk through the worlds until we find a name match and run the
+        # compress_folder function to generate the zip file archive.
+        for world in self.worlds:
+            if world.name == world_name:
+                world.sync()
+                compress_folder(os.path.join(self.env, 'archive', 'backups', 
+                                             '%s.zip' % backup_name), 
+                                os.path.join(self.env, 'env', world_name),
+                                os.path.join(self.env, 'env', world_name))
+    
+    def env_snapshot(self, snap_name=None):
+        '''snapshot [snapshot name]
+        Generates a Snapshot zip file
+        '''
+        # If there is no name specified, then go ahead and create one using
+        # the current date & time.
+        if snap_name == None:
+            snap_name = '%s_%s' % (self.name,
+                            datetime.datetime.now().strftime('%Y-%m-%d_%H%M'))
+        
+        # Now we need to build the exclusion list.  This list will contain all
+        # of the files for each world.  This is needed as we do not want to
+        # archive the world data as part of a snapshot.
+        excludes = []
+        for world in self.worlds:
+            world_path = os.path.join(self.env, 'env', world.name)
+            for dirname, dirs, files in os.walk(world_path):
+                for f in files:
+                    excludes.append(os.path.join(dirname, f))
+        
+        # And now we run the compress_folder function to generate the archive.
+        compress_folder(os.path.join(self.env, 'archive', 'snaps',
+                                     '%s.zip' % snap_name),
+                        os.path.join(self.env, 'env'),
+                        os.path.join(self.env, 'env'),
+                        excludes)
+                        
+        # Now we need to add the baskit configuration file to the snapshot.
+        zfile = ZipFile(os.path.join(self.env, 'archive', 'snaps',
+                        '%s.zip' % snap_name))
+        zfile.write(self._config_file, 'baskit.config')
+    
+    def env_snap_restore(self, snap_name):
+        '''snap_restore [snapshot name]
+        Restores a snapshot zipfile.
+        '''
+        if not self.running():
+            # If the server isn't running, then we will extract the contents
+            # of the zipfile in place of the current environment.  This means
+            # that we will first be deleting all of the existing data
+            # associated with this environment before laying the backup into
+            # it's place.
+            
+            # World exclusion list.  We love our worlds and don't wanna see
+            # them die ^_^
+            exclusion_list = []
+            for world in self.worlds:
+                world_path = os.path.join(self.env, 'env', world.name)
+                for dirname, dirs, files in os.walk(world_path):
+                    for f in files:
+                        exclusion_list.append(os.path.join(dirname, f))
+            
+            # And here comes the deleting ;)
+            for dirname, dirs, files, in os.walk(self.env):
+                for f in files:
+                    fn = os.path.join(dirname, f)
+                    if fn not in exclusion_list:
+                        os.remove(fn)
+            
+            # Now we extract the snapshot into our nice clean environment ;)
+            zfile = ZipFile(os.path.join(self.env, 'archive', 'snaps', 
+                                         '%s.zip' % snap_name))
+            zfile.extractall(os.path.join(self.env, 'env'))
+            
+            # Now that all of the data is extracted, we need to tell the
+            # environment to read it's configuration back in.  Please note
+            # that the environment names MUST match for this import to
+            # properly work.
+            self.get_config(os.path.join(self.env, 'baskit.config'))
+            self.set_config()
+            
+            # Lastly we will remove the now un-needed baskit.config file.
+            os.remove(os.path.join(self.env, 'baskit.config'))
+            return True
+        else:
+            return False
+    
+    def world_restore(self, backup_name, world_name):
+        '''world_restore [backup name]
+        Restores a world backup.  If restoring to a new world, then the
+        associated world object will be created.
+        '''
+        if not self.running():
+            # If the server isn't running, then we will extract the world
+            # backup to the world name that was provided.  We will first clean
+            # out all of the existing data to make sure that we are applying
+            # the backup cleanly.
+            
+            # First we need to check to see if there is an existing folder
+            # for the world data to reside in, if there isnt one, then create
+            # it.
+            world_path = os.path.join(self.env, 'env', world_name)
+            if not os.path.exists(world_path):
+                os.mkdir(world_path)
+            
+            # Lets clean out the current world data to make way for a nice
+            # new world.
+            for dirname, dirs, files in os.walk(world_path):
+                for f in files:
+                    os.remove(os.path.join(dirname, f))
+            
+            # Now lets unzip the backup...
+            zfile = ZipFile(os.path.join(self.env, 'archive', 'backups',
+                                         '%s.zip' % backup_name))
+            zfile.extractall(world_path)
+            
+            # Lastly lets check to see if this world name exists in our config
+            # and create the world object if needed.
+            exists = False
+            for world in self.worlds:
+                if world.name == world_name:
+                    exists = True
+            if not exists:
+                self.worlds.append(World(world_name, self.env))
+                self.set_config()
+            return True
+        else:
+            return False
