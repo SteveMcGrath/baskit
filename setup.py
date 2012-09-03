642b5a0d1a5d28aa45291c2bef7ddce4ba366202
Steven McGrath <smcgrath@tenable.com>
2012-09-03 18:36:42 -0500

    Added a setup.py file.

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+import sys
+
+setup(
+    name='Baskit',
+    version='0.2.1',
+    description='Minecraft Server Manager',
+    author='Steven McGrath',
+    author_email='steve@chigeek.com',
+    url='https://github.com/SteveMcGrath/baskit',
+    packages=['baskit', 'baskit.mc'],
+    entry_points = {
+        'console_scripts': [
+            'baskit = baskit.cli:cli',
+            ]
+    },
+    #data_files=[
+    #],
+    #install_requires=[
+    #],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Systems Administration',
+    ]
+)
