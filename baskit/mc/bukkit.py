ddfbc1ec6f529ecb3601f6fd33fdb9562650ba08
Steven McGrath <smcgrath@tenable.com>
2012-09-10 18:31:25 -0500

    Will now update bukkit builds using the API.

--- baskit/mc/bukkit.py
+++ baskit/mc/bukkit.py
@@ -1,15 +1,7 @@
 from urllib2 import urlopen
-from StringIO import StringIO
-from zipfile import ZipFile
-import re
+import json
 
-class Error(Exception):
-    def __init__(self, value):
-        self.value = value
-    def __str__(self):
-        return repr(self.value)
-
-def download(build_type='stable'):
+def download(build_type='rb'):
     '''download [build name/number]
     Downloads a bukkit java binary based on either the keywords 'stable',
     'test', or 'dev', or by specifying a build number.  The script will return
@@ -17,49 +9,23 @@ def download(build_type='stable'):
     fails.
     '''
     
-    # This is the URL dictionary information for the build_types.  This will
-    # likely need to be updated as CI changes or is retired.
-    artifact = '/artifact/*zip*/archive.zip'
-    ci = 'http://ci.bukkit.org/job/dev-CraftBukkit'
-    branches = {
-      'stable': '%s/Recommended' % ci,
-      'test': '%s/lastStableBuild' % ci,
-      'dev': '%s/lastSuccessfulBuild' % ci,
-    }
-    
-    # One of the first things that we will need to do is build the URL for the
-    # build page.  This URL will be our starting point for this whole process.
-    if build_type in ['stable', 'test', 'dev']:
-        url = branches[build_type]
-        branch = build_type
+    if build_type == 'stable':
+        build_type = 'rb'
+
+    # Thanks to the new API to talk to for CB versions, we no longer need to
+    # web scrape.
+    base = 'http://dl.bukkit.org/api/1.0/downloads/projects/craftbukkit/view'
+    if build_type in ['rb', 'beta', 'dev']:
+        url = '%s/latest-%s/' % (base, build_type)
     else:
-        branch = 'dev'
-        build = int(build_type)
-        url = '%s/%s' % (ci, build)
-    
-    # Yes I know this is ugly as we are using a try block that is accepting
-    # any error and handling it, however I admit my laziness and simply want
-    # to get this module running.
-    try:
-        title = re.compile(r'<title>.*#(\d+).*<\/title>',re.DOTALL|re.M)
-        build = int(title.findall(urlopen(url).read())[0])
-    except:
-        raise Error('Webpage does not contain version number!')
-    
-    # Now we will download the zip file that we are requesting from CI
-    data = StringIO()
-    data.write(urlopen(url + artifact).read())
-    zfile = ZipFile(data)
-    
-    # Now we will check for the JAR file and return the contents if we find
-    # them.
-    contents = None
-    for filename in zfile.namelist():
-        if filename[-3:].lower() == 'jar':
-            contents = zfile.read(filename)
+        url = '%s/build-%s/' % (base, build_type)
+
+    # Look at that, it's all pretty json now, no more 100 lines of code ^_^
+    ci = json.loads(urlopen(url).read())
+
     return {
         'server_type': 'bukkit',
-        'branch': branch,
-        'build': build,
-        'binary': contents
+        'branch': ci['channel']['slug'],
+        'build': ci['build_number'],
+        'binary': urlopen(ci['file']['url']).read()
     }
