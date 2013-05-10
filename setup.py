b8dd0196ef223802d57c3af5b367c79d56a2379e
Steven McGrath <smcgrath@tenable.com>
2013-05-09 20:24:28 -0500

    updates setup.py to v0.2.3.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 
 setup(
     name='Baskit',
-    version='0.2.3',
+    version='0.2.3.1',
     description='Minecraft Server Manager',
     author='Steven McGrath',
     author_email='steve@chigeek.com',
